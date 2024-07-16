a0085042aca0d900a9d69efce5b5faf9aeaa1f39
thernloven <>
2024-07-16 21:06:03 +0530

    refactor: updated email and number

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,26 +1,52 @@
-import { Brut1, Call, Instagram, LinkedIn, Mail, Twitter } from '../../assets/Vectors/global';
-import styles from './Hero.module.scss';
+import {
+  Brut1,
+  Call,
+  Instagram,
+  LinkedIn,
+  Mail,
+  Twitter,
+} from "../../assets/Vectors/global";
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
     <div className={styles.hero_container}>
-
       <div className={styles.header}>
-        <div className={styles.brutalist_icon}> <Brut1 /> </div>
+        <div className={styles.brutalist_icon}>
+          {" "}
+          <Brut1 />{" "}
+        </div>
         <h1>Chirag Jani</h1>
         <h2>The Last Blockchain Developer You`ll ever need.</h2>
       </div>
 
       <div className={styles.socials}>
-        <div className={styles.social_icon}> <a target='_blank' href="https://x.com/chiragjani001"> <Twitter /> </a> </div>
+        <div className={styles.social_icon}>
+          {" "}
+          <a target="_blank" href="https://x.com/chiragjani001">
+            {" "}
+            <Twitter />{" "}
+          </a>{" "}
+        </div>
         {/* <div className={styles.social_icon}><Instagram /></div> */}
-        <div className={styles.social_icon}><a target='_blank' href="https://www.linkedin.com/in/chirag-jani/"><LinkedIn /></a></div>
-        <div className={styles.social_icon}><a href="tel:+918980149260"><Call /></a></div>
-        <div className={styles.social_icon}><a href="mailto:chiragjani5901@gmail.com "><Mail /></a></div>
+        <div className={styles.social_icon}>
+          <a target="_blank" href="https://www.linkedin.com/in/chirag-jani/">
+            <LinkedIn />
+          </a>
+        </div>
+        <div className={styles.social_icon}>
+          <a href="tel:+918980149230">
+            <Call />
+          </a>
+        </div>
+        <div className={styles.social_icon}>
+          <a href="mailto:contact@chiragjani.in ">
+            <Mail />
+          </a>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/Social/Social.jsx
+++ src/Components/Social/Social.jsx
@@ -1,5 +1,5 @@
-import { Brut2 } from '../../assets/Vectors/global';
-import styles from './Social.module.scss'
+import { Brut2 } from "../../assets/Vectors/global";
+import styles from "./Social.module.scss";
 
 const Social = () => {
   return (
@@ -10,27 +10,34 @@ const Social = () => {
         </div>
       </div>
       <div className={styles.part2}>
-        <div className={styles.title}>
-          Follow Me
-        </div>
+        <div className={styles.title}>Follow Me</div>
         <div className={styles.options}>
-          <span><a target='_blank' href="https://x.com/chiragjani001">Twitter</a></span>
-          <span><a target='_blank' href="https://www.linkedin.com/in/chirag-jani/">LinkedIn</a></span>
+          <span>
+            <a target="_blank" href="https://x.com/chiragjani001">
+              Twitter
+            </a>
+          </span>
+          <span>
+            <a target="_blank" href="https://www.linkedin.com/in/chirag-jani/">
+              LinkedIn
+            </a>
+          </span>
           {/* <span><a href="">Instagram</a></span> */}
         </div>
-
       </div>
       <div className={styles.part3}>
-        <div className={styles.title}>
-          Lets Work Together
-        </div>
+        <div className={styles.title}>Lets Work Together</div>
         <div className={styles.options}>
-          <span><a href="tel:+918980149260">+91 8980149260</a></span>
-          <span><a href="mailto:chiragjani5901@gmail.com">chiragjani5901@gmail.com</a></span>
+          <span>
+            <a href="tel:+918980149230">+91 8980149260</a>
+          </span>
+          <span>
+            <a href="mailto:contact@chiragjani.in">contact@chiragjani.in</a>
+          </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Social;
